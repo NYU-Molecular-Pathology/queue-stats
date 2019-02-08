397c53c8c1c4a63fd2663865e0f0f9d4b8de0c4c
Stephen Kelly <stevekm@users.noreply.github.com>
2019-02-08 12:08:57 -0500

    add qavail script

---
+++ qavail.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Get SLURM availability summary
+"""
+import util.slurm as slurm
+import csv
+import sys
+n = slurm.Nodes()
+
+writer = csv.DictWriter(sys.stdout, delimiter = '\t', fieldnames = n.avail[0].keys())
+writer.writeheader()
+for entry in n.avail:
+    writer.writerow(entry)

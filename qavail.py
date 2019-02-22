30564fe8f106293cf3a8c7ccef1b1f535cfd24ce
Stephen Kelly <stevekm@users.noreply.github.com>
2019-02-21 20:31:12 -0500

    add pcnt mem used to qavail output

--- qavail.py
+++ qavail.py
@@ -16,12 +16,22 @@ import sys
 args = ['sinfo', '-O', 'allocmem,freemem,nodehost,partition,cpus,sockets,memory,gres,cpusstate,reason,statelong']
 n = slurm.Nodes(command = args)
 
-writer = csv.DictWriter(sys.stdout, delimiter = '\t', fieldnames = n.avail[0].keys())
+# fieldnames = n.avail[0].keys()
+fieldnames = ['node', 'allocmem', 'freemem', 'pcnt_mem', 'cpu', 'state', 'partitions']
+
+writer = csv.DictWriter(sys.stdout, delimiter = '\t', fieldnames = fieldnames)
 writer.writeheader()
 for entry in n.avail:
     # clean up for printing
     if 'state' in entry:
+        # truncate state
         entry['state'] = entry['state'][0:5]
     if 'node' in entry:
+        # pad node name
         entry['node'] = "{} ".format(entry['node'])
+    # calculate percent mem
+    entry['pcnt_mem'] = float(entry['allocmem']) / float(entry['totalmem']) * 100
+    entry['pcnt_mem'] = "{0:.1f}".format(entry['pcnt_mem'])
+    entry.pop('totalmem', None)
+
     writer.writerow(entry)
